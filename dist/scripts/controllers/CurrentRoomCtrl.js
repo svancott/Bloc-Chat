2fc06f5379bdad8bf804bb1a7ebc7256588a9f5e
svancott <thevanicotti@gmail.com>
2017-01-18 12:36:56 -0200

    room name and messages displaying

--- dist/scripts/controllers/CurrentRoomCtrl.js
+++ dist/scripts/controllers/CurrentRoomCtrl.js
@@ -1,21 +1,38 @@
 (function() {
-     function CurrentRoomCtrl($state, Room) {
-        var vm = this;
+     function CurrentRoomCtrl($state, Room, $firebaseArray, $firebaseObject) {
+        var ref = firebase.database().ref("messages");
+		var vm = this;
 		vm.roomId = $state.params.roomId;
+		var roomsRef = firebase.database().ref("rooms");
+		vm.getRoomName = function(roomId){
+			vm.roomName = $firebaseObject(roomsRef.child(roomId));
+			return vm.roomName
+			console.log(roomId)
+		} 
 		vm.getCurrentRoom = function() {
 			vm.currentRoom = Room.getRoom(vm.roomId);
 			console.log("current room:" + vm.currentRoom);
 		}
+		
 			
 //			function() {
 //			// Pass this to Firebase
 //			vm.currentRoom = Room.getRoom();
 //			console.log("current room:" + vm.currentRoom)
 //		}
+		
+		vm.getRoom = function(roomId) {
+			
+			
+			vm.room = $firebaseArray(ref.child(roomId));
+			
+			return vm.room;
+			console.log(vm.room);
+		}
 
      }
 
      angular
          .module('blocChat')
-         .controller('CurrentRoomCtrl', ['$state', 'Room', CurrentRoomCtrl]);
+         .controller('CurrentRoomCtrl', ['$state', 'Room', '$firebaseArray', '$firebaseObject', CurrentRoomCtrl]);
  })();
